6bcb2df99318eff7fef3ed31a273fdacddbe94c6
Steve Kinney <hello@stevekinney.net>
2019-03-07 14:30:54 +0000

    With keys!

--- app/renderer.js
+++ app/renderer.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
+import { ENETDOWN } from 'constants';
 
 class Application extends Component {
   state = {
     newClipping: '',
+    clippings: [],
   }
 
   handleChange = (event) => {
@@ -11,11 +13,29 @@ class Application extends Component {
     this.setState({ [name]: value });
   }
 
+  handleSubmit = (event) => {
+    event.preventDefault();
+    const clipping = {
+      id: Date.now(),
+      content: this.state.newClipping,
+    }
+    const clippings = this.state.clippings.concat(clipping);
+    this.setState({
+      clippings,
+      newClipping: '',
+    });
+  }
+
   render() {
-    const { newClipping } = this.state;
+    const { newClipping, clippings } = this.state;
     return (
       <section className="content">
-        <input type="text" placeholder="New Clipping" value={newClipping} name="newClipping" onChange={this.handleChange} />
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" placeholder="New Clipping" value={newClipping} name="newClipping" onChange={this.handleChange} />
+        </form>
+        <div>
+          {clippings.map(clipping => <li key={clipping.id}>{clipping.content}</li>)}
+        </div>
       </section>
     )
   }
